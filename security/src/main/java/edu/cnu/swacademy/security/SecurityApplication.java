f2085f9d5237b9a08382ee380267a758d594f5ed
leegyeongsik <leegs1012@naver.com>
2025-09-12 13:12:32 +0900

    init

--- security/src/main/java/edu/cnu/swacademy/security/SecurityApplication.java
+++ security/src/main/java/edu/cnu/swacademy/security/SecurityApplication.java
@@ -1,8 +1,5 @@
 package edu.cnu.swacademy.security;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
